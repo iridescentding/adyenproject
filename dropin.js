51e65a439503291419f0b393a34122c0a463e139
Jay Ding <55494341+iridescentding@users.noreply.github.com>
2025-02-25 03:32:50 +0800

    Add files via upload
    deployed success

---
+++ dropin.js
@@ -0,0 +1,72 @@
+/**
+ * IMPORTANT - Set a boolean indicating whether index.html is loading a version of adyen.js (& adyen.css) >= 5.0.0
+ */
+const head = document.head.innerHTML;
+const version = head.substring(head.indexOf('sdk/') + 4, head.indexOf('/adyen'));
+const majorVn = Number(version.substring(0, version.indexOf('.')));
+const IS_VERSION_5 = majorVn >= 5;
+
+const DEFAULT_COUNTRY = 'US';
+
+// 0. Get clientKey
+getClientKey().then(clientKey => {
+    const urlParams = getSearchParameters(window.location.search);
+
+    // Can add request params to this object
+    const pmReqConfig = {countryCode: urlParams.countryCode || DEFAULT_COUNTRY};
+    getPaymentMethods(pmReqConfig).then(async paymentMethodsResponse => {
+
+        paymentMethodsResponse.paymentMethods.reverse();
+
+        let allowedPMS = urlParams.allowedpms;// e.g. &allowedpms=[scheme,ideal]
+        if(allowedPMS === '[]' || typeof allowedPMS === 'undefined') allowedPMS = [];// if empty, all PMs will show
+
+        const configObj = {
+            environment: 'test',
+            clientKey: clientKey,
+            paymentMethodsResponse,
+            allowPaymentMethods: allowedPMS,
+            paymentMethodsConfiguration: {
+                card: {
+                    hasHolderName: true,
+                    holderNameRequired: true,
+                    billingAddressRequired: true,
+                    billingAddressMode: "full"
+                }
+            },
+            onChange: state => {
+                updateStateContainer(state);
+            },
+            onSubmit: (state, dropin) => {
+                if (state.isValid) {
+                    makePayment({
+                        ...state.data,
+                        reference: 'shiyuanding_adyenrecruitment',
+                        returnUrl: 'https://docs.adyen.com/'
+                    });
+                }
+            }
+        };
+        
+        
+
+        // 1. Create an instance of AdyenCheckout
+        if (!IS_VERSION_5) {
+            window.checkout = new AdyenCheckout(configObj);
+        } else {
+            window.checkout = await AdyenCheckout(configObj);
+        }
+
+        // 2. Create and mount the Component
+        window.dropin = checkout
+            .create('dropin', {
+                // Events
+                onSelect: activeComponent => {
+                    if (activeComponent.state && activeComponent.state.data) updateStateContainer(activeComponent.data); // Demo purposes only
+                },
+                showStoredPaymentMethods: false
+            })
+            .mount('#dropin-container');
+
+    });
+});
